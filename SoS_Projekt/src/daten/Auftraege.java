452a7ec6d7c338ee0c3e68e6d0dff18ceed48637
Rosee9 <12ekem1bwi@hft-stuttgart.de>
2022-05-13 13:01:00 +0200

    Aufträge.java -  gefixt

--- SoS_Projekt/src/daten/Auftraege.java
+++ SoS_Projekt/src/daten/Auftraege.java
@@ -4,14 +4,12 @@ package daten;
 
 import java.util.ArrayList;
 
-import Kunde.Kunde;
-import Rechner.Komponenten;
-import Rechner.Rechner;
+
 
 public class Auftraege{
 	
 	
-	ArrayList<Komponenten> rechner;
+	ArrayList<Komponente> rechner;
 	private String kunde;
 	private int auftragsNummer;
 	private String status;
@@ -19,7 +17,7 @@ public class Auftraege{
 	
 	
 
-	public Auftraege(ArrayList<Komponenten> rechner, Kunde kunde, int auftragsNummer, String status) {
+	public Auftraege(ArrayList<Komponente> rechner, Kunde kunde, int auftragsNummer, String status) {
 		this.rechner=rechner;
 		this.kunde = kunde.getname();
 		this.auftragsNummer = auftragsNummer;
