d330ae04ab8f9b3c86deb4d3e810cdc09236cc2c
julianraubald <julianraubald@julians-macbook-pro.fritz.box>
2022-05-19 21:02:49 +0200

    Aufträge

--- SoS_Projekt/src/daten/Auftraege.java
+++ SoS_Projekt/src/daten/Auftraege.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 public class Auftraege{
 	
 	
-	ArrayList<Komponente> rechner;
+	private ArrayList<Komponente> rechner;
 	private String kunde;
-	private int auftragsNummer;
+	private String auftragsNummer;
 	private String status;
 	
 	
 	
 
-	public Auftraege(ArrayList<Komponente> rechner, Kunde kunde, int auftragsNummer, String status) {
+	public Auftraege(ArrayList<Komponente> rechner, Kunde kunde, String auftragsNummer, String status) {
 		this.rechner=rechner;
 		this.kunde = kunde.getname();
 		this.auftragsNummer = auftragsNummer;
@@ -30,10 +30,10 @@ public class Auftraege{
 	public void setKunde(String kunde) {
 		this.kunde = kunde;
 	}
-	public int getAuftragsNummer() {
+	public String getAuftragsNummer() {
 		return auftragsNummer;
 	}
-	public void setAuftragsNummer(int auftragsNummer) {
+	public void setAuftragsNummer(String auftragsNummer) {
 		this.auftragsNummer = auftragsNummer;
 	}
 	public String getStatus() {
@@ -41,6 +41,14 @@ public class Auftraege{
 	}
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	
+	public ArrayList<Komponente> getRechner() {
+		return rechner;
+	}
+
+	public void setRechner(ArrayList<Komponente> rechner) {
+		this.rechner = rechner;
 	}
 	
 
